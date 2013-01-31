7fbc2f876ba29c58e42e52fd8d765f2997cfdd08
Keith M Wesolowski <wesolows@foobazco.org>
2013-01-31 19:32:37 +0000

    wesolows/node-snmpjs#6 Question on how to listen for traps

---
+++ tl.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+/*
+ * Copyright (c) 2013, Joyent, Inc. All rights reserved.
+ */
+
+var snmp = require('./lib/index.js');
+var mib = require('./lib/mib/index.js');
+var bunyan = require('bunyan');
+var fs = require('fs');
+var util = require ('util');
+
+var config = process.argv[2] || 'tl.json';
+var cfstr = fs.readFileSync(config);
+var cf, log_cf;
+var log, tl;
+
+cf = JSON.parse(cfstr);
+log_cf = cf.log || {
+	name: 'snmpd',
+	level: 'trace'
+};
+
+log = new bunyan(log_cf);
+
+tl = snmp.createTrapListener({
+	log: log
+});
+
+tl.bind({ family: 'udp4', port: 162 });
+tl.on('trap', function (msg) {
+	console.log(util.inspect(snmp.message.serializer(msg), false, null));
+});
